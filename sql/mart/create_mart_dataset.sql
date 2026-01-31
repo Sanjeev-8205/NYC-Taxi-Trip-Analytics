e591961cc6d1307634633a0397e228aac7136636
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-01-31 23:56:45 +0530

    Create create_mart_dataset.sql

---
+++ sql/mart/create_mart_dataset.sql
@@ -0,0 +1,5 @@
+--create_mart_dataset.sql
+--Purpose: To create the mart dataset which stores many business-ready mart tables.
+--Source: stg.events
+
+CREATE SCHEMA IF NOT EXISTS `nyc-taxi-485918.mart`;
