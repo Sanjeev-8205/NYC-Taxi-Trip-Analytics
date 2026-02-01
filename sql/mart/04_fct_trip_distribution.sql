2536889fbbd4cb7a014302e7945f554b5999841f
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-02 01:59:53 +0530

    Update 04_fct_trip_distribution.sql

--- sql/mart/04_fct_trip_distribution.sql
+++ sql/mart/04_fct_trip_distribution.sql
@@ -6,14 +6,14 @@
 CREATE OR REPLACE TABLE `nyc-taxi-485918.mart.fct_trip_distribution` AS
 SELECT
   CASE
-    WHEN trip_distance>=0 AND trip_distance<1 THEN '0-1 mile'
-    WHEN trip_distance>=1 AND trip_distance<3 THEN '1-3 miles'
-    WHEN trip_distance>=3 AND trip_distance<5 THEN '3-5 miles'
-    WHEN trip_distance>=5 AND trip_distance<10 THEN '5-10 miles'
-    WHEN trip_distance>=10 THEN '10+ miles'
+    WHEN trip_distance>=0 AND trip_distance<1.5 THEN '0-1.5 mile'
+    WHEN trip_distance>=1.5 AND trip_distance<4 THEN '1.5-4 miles'
+    WHEN trip_distance>=4 AND trip_distance<13 THEN '4-13 miles'
+    WHEN trip_distance>=13 THEN '13+ miles'
   END AS buckets,
   pickup_zone_name,
   COUNT(*) AS trip_count,
+  AVG(trip_distance) AS avg_trip_distance,
   AVG(total_amount) AS avg_revenue,
   AVG(fare_amount) AS avg_fare,
   AVG(trip_duration_minutes) AS avg_trip_duration_minutes
