9590c4a1c9d34bf0979f909969d9ec8b28e86ce1
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-01 23:18:25 +0530

    Update 03_fct_zone_efficiency.sql

--- sql/mart/03_fct_zone_efficiency.sql
+++ sql/mart/03_fct_zone_efficiency.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE TABLE `nyc-taxi-485918.mart.fct_zone_efficiency` AS
 SELECT
   pickup_zone_name,
   pickup_date,
+  COUNT(*) AS total_trips,
   SAFE_DIVIDE(SUM(total_amount), COUNT(*)) AS revenue_per_trip,
   SAFE_DIVIDE(SUM(total_amount),SUM(trip_distance)) AS revenue_per_mile,
   SAFE_DIVIDE(SUM(fare_amount), SUM(trip_distance)) AS fare_per_miles,
