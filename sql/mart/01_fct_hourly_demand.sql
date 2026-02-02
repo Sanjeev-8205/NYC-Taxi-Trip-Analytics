4ac58c53f3eabab5cace766797eb6e8fe386692a
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-03 01:40:54 +0530

    Update 01_fct_hourly_demand.sql

--- sql/mart/01_fct_hourly_demand.sql
+++ sql/mart/01_fct_hourly_demand.sql
@@ -7,10 +7,11 @@ CREATE OR REPLACE TABLE `nyc-taxi-485918.mart.fct_hourly_demand` AS
 SELECT
   pickup_date,
   pickup_hour,
+  pickup_weekday,
   COUNT(*) AS total_trip,
   SUM(total_amount) AS total_revenue,
   AVG(fare_amount) AS avg_fare,
   AVG(trip_duration_minutes) AS avg_trip_duration,
   AVG(trip_distance) AS avg_trip_distance
 FROM `nyc-taxi-485918.stg.events`
-GROUP BY pickup_date, pickup_hour
+GROUP BY pickup_date, pickup_hour, pickup_weekday
