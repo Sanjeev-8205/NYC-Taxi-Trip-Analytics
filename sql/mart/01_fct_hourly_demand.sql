da8136b6b87ccb967b073ee32bd0bfa90b68c2e5
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-06 03:46:27 +0530

    Update 01_fct_hourly_demand.sql

--- sql/mart/01_fct_hourly_demand.sql
+++ sql/mart/01_fct_hourly_demand.sql
@@ -1,7 +1,7 @@
 --01_fct_hourly_demand.sql
 --Purpose: To understand the hourly demand of the neywork taxi in 2021.
 --Grain: 1 row per unique pickup_date, pickup_hour combination
---Source: stg.events
+--Source: stg.events_view_dedup
 
 CREATE OR REPLACE TABLE `nyc-taxi-485918.mart.fct_hourly_demand` AS
 SELECT
@@ -13,5 +13,5 @@ SELECT
   AVG(fare_amount) AS avg_fare,
   AVG(trip_duration_minutes) AS avg_trip_duration,
   AVG(trip_distance) AS avg_trip_distance
-FROM `nyc-taxi-485918.stg.events`
+FROM `nyc-taxi-485918.stg.events_view_dedup`
 GROUP BY pickup_date, pickup_hour, pickup_weekday
