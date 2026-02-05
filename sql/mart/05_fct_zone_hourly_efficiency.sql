eeb9391ea3d3cfc58f297740325a848de354629a
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-06 04:10:16 +0530

    Update 05_fct_zone_hourly_efficiency.sql

--- sql/mart/05_fct_zone_hourly_efficiency.sql
+++ sql/mart/05_fct_zone_hourly_efficiency.sql
@@ -1,7 +1,7 @@
 --05_fct_zone_hourly_efficiency.sql
 --Purpose: To aggregate the zone's hourly efficiency.
 --Grain: 1 row per unique combination of pickup_zone_name, pickup_hour, day_of_week
---Source: stg.events
+--Source: stg.events_view_dedup
 --Note: 
 -- -The need for this mart table arised after the worst results obtained from the taxi allocation baseline.
 -- -Furthermore, the 2nd version produced better taxi allocation results due to (zone x hour) level grain.
@@ -18,5 +18,5 @@ SELECT
   SUM(trip_duration_minutes) AS trip_duration,
   SAFE_DIVIDE(SUM(total_amount), SUM(trip_duration_minutes)) AS revenue_per_minute,
   SAFE_DIVIDE(SUM(trip_duration_minutes), COUNT(*)) AS avg_trip_duration
-FROM `nyc-taxi-485918.stg.events`
+FROM `nyc-taxi-485918.stg.events_view_dedup`
 GROUP BY pickup_zone_name, pickup_hour, day_of_week
