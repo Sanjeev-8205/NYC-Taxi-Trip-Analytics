cab2edea4d7d2334393eb9e08d01bc360faa804c
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-06 04:09:49 +0530

    Update 04_fct_trip_distribution.sql

--- sql/mart/04_fct_trip_distribution.sql
+++ sql/mart/04_fct_trip_distribution.sql
@@ -1,7 +1,7 @@
 --04_fct_trip_distribution.sql
 --Purpose: To find the short-long trips and cheap-expensive trips
 --Grain: 1 row per unique combination of buckets, pickup_zone_name
---Source: stg.events
+--Source: stg.events_view_dedup
 
 CREATE OR REPLACE TABLE `nyc-taxi-485918.mart.fct_trip_distribution` AS
 SELECT
@@ -17,6 +17,6 @@ SELECT
   AVG(total_amount) AS avg_revenue,
   AVG(fare_amount) AS avg_fare,
   AVG(trip_duration_minutes) AS avg_trip_duration_minutes
-FROM `nyc-taxi-485918.stg.events`
+FROM `nyc-taxi-485918.stg.events_view_dedup`
 WHERE trip_distance IS NOT NULL
 GROUP BY buckets, pickup_zone_name
