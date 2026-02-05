5596b4a2f3d237c46853b61f78845f8e09c6b5d5
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-06 03:29:18 +0530

    Create 06_metric_negative_records_check.sql

---
+++ DQ_check/06_metric_negative_records_check.sql
@@ -0,0 +1,17 @@
+-- 06_metric_negative_records_check.sql
+
+-- CONTRACT:
+-- Core aggregated metrics must never be negative.
+-- Any violation indicates issues in aggregation ore modeling.
+-- The affected records are excluded from the results.
+
+-- REQUIRED INPUTS:
+-- table: mart.fct_hourly_demand
+-- critical_columns: total_revenue, avg_fare, avg_trip_duration, avg_trip_distance
+
+SELECT
+  COUNTIF(total_revenue<0) AS negative_revenue,
+  COUNTIF(avg_fare<0) AS negative_fare,
+  COUNTIF(avg_trip_duration<0) AS negative_duration,
+  COUNTIF(avg_trip_distance<0) AS negative_duration
+FROM `nyc-taxi-485918.mart.fct_hourly_demand`
