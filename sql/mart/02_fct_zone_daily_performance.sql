d762228544b2801a4bec67036826c3f8cc147eb3
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-06 03:47:26 +0530

    Update 02_fct_zone_daily_performance.sql

--- sql/mart/02_fct_zone_daily_performance.sql
+++ sql/mart/02_fct_zone_daily_performance.sql
@@ -1,7 +1,7 @@
 --02_fct_zone_daily_performance.sql
 --Purpose: To aggregate the daily zone performance.
 --Grain: 1 row per unique pickup_zone_name, pickup_borough, pickup_date combinations
---Source: stg.events
+--Source: stg.events_view_dedup
 --Note: This mart table is capable of finding the best zones and worst zones based on revenue, total_trips and so on.
 
 CREATE OR REPLACE TABLE `nyc-taxi-485918.mart.fct_zone_daily_performance` AS
@@ -15,5 +15,5 @@ SELECT
   AVG(trip_distance) AS avg_distance,
   AVG(trip_duration_minutes) AS avg_trip_duration,
   COUNT(*)/24 AS trips_per_hour
-FROM `nyc-taxi-485918.stg.events`
+FROM `nyc-taxi-485918.stg.events_view_dedup`
 GROUP BY pickup_zone_name, pickup_borough, pickup_date
