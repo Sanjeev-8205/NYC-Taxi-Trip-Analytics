55aa6447a3618c7594d101303eb900deea2cc696
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-02-07 17:11:15 +0530

    Create 5_hourly_taxi_demand_predictions.sql

---
+++ sql/ML/5_hourly_taxi_demand_predictions.sql
@@ -0,0 +1,20 @@
+-- 5_hourly_taxi_demand_predictions.sql
+-- Purpose: To predict the taxi demand on October 1,2021 and then store them in a table.
+-- Note:
+-- -This is a very important table as this stores the predictions odf the model.
+-- -These predictions are used in taxi allocation query to allocate the taxis for the specific date.
+
+CREATE OR REPLACE TABLE `nyc-taxi-485918.analytics.hourly_taxi_demand_predictions` AS
+SELECT
+  pickup_zone_name,
+  predicted_total_trip AS predicted_trips,
+  pickup_date AS prediction_date,
+  MOD(pickup_hour+1, 24) AS predicted_hour,
+FROM ML.PREDICT(
+  MODEL `nyc-taxi-485918.ML.model_predict_trip`,
+  (
+  SELECT *
+  FROM `nyc-taxi-485918.ML.mart_ml_trip_features`
+  WHERE pickup_date='2021-10-01'
+  )
+)
